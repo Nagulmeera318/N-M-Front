8ad3c6966b7aa2c0ccedd3bcb9134c5fcc946320
Nagulmeera318 <nagulm512@gmail.com>
2024-07-06 22:33:24 +0530

    contact info

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import Products from './components/Products';
+// src/App.js
+import React from 'react';
+import Header from './components/Header';
+import Contact from './components/Contact';
 import './App.css';
 
 function App() {
-  return ( 
-    <div className="App"> 
-    <Products/>
+  return (
+    <div className="App">
+      <div className="Content">
+        <Header />
+      </div>
+      <footer className="Footer">
+        <Contact />
+      </footer>
     </div>
   );
 }
